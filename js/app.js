4b6cc50d64737ef8f8a9e871acbaddb3c875e1af
Lesley Rivera <les.aguilar@live.com>
2020-03-28 20:35:50 -0700

    lab03

--- js/app.js
+++ js/app.js
@@ -1,34 +1,20 @@
 'use strict';
 
-
-const keyArr = [];
-
+let keyArr = [];
 
 function Creature(object) {
-  this.image_url = object.image_url;
+  this.url = object.image_url;
   this.title = object.title;
   this.description = object.description;
   this.keyword = object.keyword;
   this.horns = object.horns;
 }
 
-Creature.prototype.render = function () {
-  let $creatureClone = $('.photo-template').clone();
-  $creatureClone.find('h2').text(this.title);
-  $creatureClone.find('img').attr('src', this.image_url);
-  $creatureClone.find('img').attr('alt', this.title);
-  $creatureClone.find('p').text(this.description);
-  $creatureClone.removeClass('photo-template');
-  $creatureClone.attr('class', this.keyword);
-  $('main').append($creatureClone);
-
-};
-
 function showImages() {
   let $chosen = $(this).val();
   if ($chosen === 'default') {
     $('section').fadeIn();
-    $('.photo-template').hide();
+    // $('.photo-template').hide();
   } else {
     $('section').hide();
     $('.' + $chosen).fadeIn();
@@ -42,25 +28,45 @@ function appendDropDown(keyword) {
 }
 function appendToKeyArr() {
   keyArr.sort();
-  for ( let i = 0; i < keyArr.length; i++) {
+  for (let i = 0; i < keyArr.length; i++) {
     $('select').append(`<option value="${keyArr[i]}">${keyArr[i]}</option>`);
   }
 }
 
-function loadData() {
-  $.ajax('/data/page-1.json')
+function renderCreats(object, sourceID, target) {
+  let $target = $(target);
+  let templateMarkUp = $(sourceID).html();
+  let newMarkUp = Mustache.render(templateMarkUp, object);
+  $target.append(newMarkUp);
+
+}
+
+function loadData(getFile) {
+  $.ajax(getFile)
     .then(data => {
       data.forEach((object, indx) => {
         let creature = new Creature(object);
-        creature.render();
+        renderCreats(creature, '#pageone-template', '.targets');
         appendDropDown(object.keyword);
       });
       appendToKeyArr();
     });
 }
 
+function displayPage(getFile) {
+  keyArr = [];
+  $('section').remove();
+  loadData(getFile);
+}
+
 $(document).ready(function () {
-  $('select').on('change', showImages );
-  loadData();
+  $('select').on('change', showImages);
+  $('#button').on('click', function () {
+    displayPage('data/page-1.json');
+  });
+  $('#button2').on('click', function () {
+    displayPage('data/page-2.json');
+  });
+  displayPage('data/page-1.json');
 });
 
